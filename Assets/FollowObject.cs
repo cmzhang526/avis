02f54bb4395fd0f1fefae6267e47981320a84b11
Colin Spiridonov <spiridon@usc.edu>
2020-10-16 20:42:56 -0700

    Added camera follow script

---
+++ Assets/FollowObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowObject : MonoBehaviour
+{
+    public GameObject targetObject;
+
+    private Vector3 displacement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        displacement = targetObject.transform.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = targetObject.transform.position - displacement;
+        // transform.LookAt(targetObject.transform);
+    }
+}
